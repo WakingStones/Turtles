6caab04083bf9ee0e46be61752956f3d9bc246ff
Lord_Ralex <lordralex@gmail.com>
2018-06-23 12:08:24 -0500

    Have build now produce release-ready files

--- src/main/java/org/wakingstones/turtles/core/Main.java
+++ src/main/java/org/wakingstones/turtles/core/Main.java
@@ -2,12 +2,23 @@ package org.wakingstones.turtles.core;
 
 import net.minecraft.launchwrapper.Launch;
 
+import java.io.File;
+
 public class Main {
 
+    private static final String[] DUPE_LIBRARIES = new String[]{
+            "gson-2.2.3.jar",
+            "guava-15.0.jar"
+    };
+
     public static void main(String[] args) {
-        String[] a = new String[] {
-                "--tweakClass","org.wakingstones.turtles.core.CallersBaneTweaker"
+        String[] a = new String[]{
+                "--tweakClass", "org.wakingstones.turtles.core.CallersBaneTweaker"
         };
+
+        for (String s : DUPE_LIBRARIES) {
+            new File("libs", s).delete();
+        }
 
         Launch.main(a);
     }
