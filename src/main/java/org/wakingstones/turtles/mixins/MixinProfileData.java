7897a0d5c50fcc07843ce2a2aeb55335c3e9922a
Lord_Ralex <lordralex@gmail.com>
2018-07-01 14:57:06 -0500

    Bring in fix about system accounts to avoid mistakes

--- src/main/java/org/wakingstones/turtles/mixins/MixinProfileData.java
+++ src/main/java/org/wakingstones/turtles/mixins/MixinProfileData.java
@@ -9,11 +9,11 @@ public abstract class MixinProfileData {
 
     @ModifyConstant(remap = false, method = "getTopRanked")
     private static String fixGetTopRanked(String input) {
-        return "SELECT P.name, PD.alpha_ranked * PD.real_ranked AS rating FROM profiles P INNER JOIN profile_data PD ON PD.profile_id = P.id ORDER BY rating DESC";
+        return "SELECT P.name, PD.alpha_ranked * PD.real_ranked AS rating FROM profiles P INNER JOIN profile_data PD ON PD.profile_id = P.id WHERE p.admin_role != 'System' AND p.user_uuid NOT IN ('@limited', '@marketplace', 'Shopkeeper', 'Upgrade') ORDER BY rating DESC";
     }
 
     @ModifyConstant(remap = false, method = "getTopRankedProfiles")
     private static String fixGetTopRankedProfiles(String input) {
-        return "SELECT p.* FROM profile_data PD INNER JOIN profiles p ON p.id = PD.profile_id ORDER BY (pd.alpha_ranked * pd.real_ranked) DESC";
+        return "SELECT p.* FROM profile_data PD INNER JOIN profiles p ON p.id = PD.profile_id WHERE p.admin_role != 'System' AND p.user_uuid NOT IN ('@limited', '@marketplace', 'Shopkeeper', 'Upgrade') ORDER BY (pd.alpha_ranked * pd.real_ranked) DESC";
     }
 }
