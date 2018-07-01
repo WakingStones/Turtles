d0498907fd04719269ecdd39bba9f985b7e1980f
Lord_Ralex <lordralex@gmail.com>
2018-07-01 14:11:42 -0500

    Apply fix to second top

--- src/main/java/org/wakingstones/turtles/mixins/MixinProfileData.java
+++ src/main/java/org/wakingstones/turtles/mixins/MixinProfileData.java
@@ -2,18 +2,18 @@ package org.wakingstones.turtles.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import platform.model.entity.BaseEntity;
 import platform.model.entity.ProfileData;
-import platform.model.entity.ProfileRating;
-
-import java.util.List;
 
 @Mixin(ProfileData.class)
-public abstract class MixinProfileData extends BaseEntity<Integer> {
+public abstract class MixinProfileData {
 
     @ModifyConstant(remap = false, method = "getTopRanked")
-    private static String fixSql(String input) {
+    private static String fixGetTopRanked(String input) {
         return input + " WHERE p.admin_role != 'System' AND p.user_uuid NOT IN ('@limited', '@marketplace', 'Shopkeeper', 'Upgrade')";
     }
 
+    @ModifyConstant(remap = false, method = "getTopRankedProfiles")
+    private static String fixGetTopRankedProfiles(String input) {
+        return input + " WHERE p.admin_role != 'System' AND p.user_uuid NOT IN ('@limited', '@marketplace', 'Shopkeeper', 'Upgrade')";
+    }
 }
